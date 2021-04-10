b6ef0389048842cadbb66ecf02d6da95bb54ebf5
Andy Shand <andrewshand94@gmail.com>
2021-04-10 14:21:09 +0100

    Add message about "Play from Selection" not working inside clip view

--- extra-resources/default-mods/playFromSelection.bitwig.js
+++ extra-resources/default-mods/playFromSelection.bitwig.js
@@ -10,72 +10,79 @@ transport.getOutPosition().markInterested()
 transport.isPlaying().markInterested()
 transport.isArrangerLoopEnabled().markInterested()
 
+const cursorClip = host.createArrangerCursorClip(1, 1)
+cursorClip.getPlayStart().markInterested()
+cursorClip.getLoopStart().markInterested()
+cursorClip.getLoopLength().markInterested()
+cursorClip.isLoopEnabled().markInterested()
+
 let startOnMouseDown = 0
 
 function saveLoop() {
     const loopEnabled = transport.isArrangerLoopEnabled().get()
     const currLoopStart = transport.getInPosition().get()
     const currLoopEnd = transport.getOutPosition().get()
+
+    const clipStart = cursorClip.getPlayStart().get()
+    const clipLoopStart = cursorClip.getLoopStart().get()
+    const clipLoopLength = cursorClip.getLoopLength().get()
+    const clipLoopEnabled = cursorClip.isLoopEnabled().get()
+
     return {
         loopEnabled,
         currLoopStart,
-        currLoopEnd
+        currLoopEnd,
+
+        clipStart,
+        clipLoopStart,
+        clipLoopLength,
+        clipLoopEnabled
     }
 }
-function restoreLoop(obj) {
-    transport.isArrangerLoopEnabled().set(obj.loopEnabled)
-    transport.getInPosition().set(obj.currLoopStart)
-    transport.getOutPosition().set(obj.currLoopEnd)
+function restoreLoop(obj, restoreClip = false) {
+    if (!restoreClip) {
+        transport.isArrangerLoopEnabled().set(obj.loopEnabled)
+        transport.getInPosition().set(obj.currLoopStart)
+        transport.getOutPosition().set(obj.currLoopEnd)
+    } else {
+        cursorClip.getPlayStart().set(obj.clipStart)
+        cursorClip.getLoopStart().set(obj.clipLoopStart)
+        cursorClip.getLoopLength().set(obj.clipLoopLength)
+        cursorClip.isLoopEnabled().set(obj.clipLoopEnabled)
+    }
 }
 
 packetManager.listen('play-from-selection', (packet) => {
-    if (packet.data) {
-        // Support for clicking to set playhead 
-        if (transport.isPlaying().get()) {
-            return
-        }
+    log('hello!')
+    let doIt = () => {
+        const saved = saveLoop()
+        
+        runAction("Loop Selection")
+        runAction("jump_to_beginning_of_arranger_loop")
+        transport.play()
 
-        let mouseup = packet.data.mouseup
-        let mousedown = packet.data.mousedown
+        setTimeout(() => {
+            if (saved.clipLoopStart !== cursorClip.getLoopStart().get() 
+            || saved.clipLoopLength !== cursorClip.getLoopLength().get()
+            || saved.clipLoopEnabled !== cursorClip.isLoopEnabled().get()) {
+                // We were inside the clip view, because the meaning of "Loop Selection" changed to 
+                // looping the internal contents of a clip rather in the arranger
+    
+                showMessage(`"Play From Selection" does not currently support the clip view, sorry!`)
+                // transport.playStartPosition().set(saved.clipStart + saved.clipLoopStart)
+                // transport.play()
+                restoreLoop(saved, true)
+            } 
+        }, 100)
 
-        if (mousedown) {
-            startOnMouseDown = transport.playStartPosition().get()
-        } else if (mouseup) {
-            setTimeout(() => {
-                const startNow = transport.playStartPosition().get()
-                if (startNow === startOnMouseDown) {
-                    // The user didn't do something to change transport, try changing it to selected item
-                    const saved = saveLoop()
-                    runAction("Loop Selection")
-                    runAction("jump_to_beginning_of_arranger_loop")
-                    
-                    setTimeout(() => {
-                        if (transport.playStartPosition().get() === 0) {
-                            // There was nothing selected, revert!
-                            transport.playStartPosition().set(startNow)
-                        }
-                    }, 100)
-                    restoreLoop(saved)
-                }
-            }, 0)
-        }
-    } else {
-        let doIt = () => {
-            const saved = saveLoop()
-            
-            runAction("Loop Selection")
-            runAction("jump_to_beginning_of_arranger_loop")
-            transport.play()
-            
-            restoreLoop(saved)
-        }
-        if (transport.isPlaying().get()) {
-            transport.stop()
-            setTimeout(doIt, 100)
-        } else {
-            doIt()
-        }
+        restoreLoop(saved)
     }
+    if (transport.isPlaying().get()) {
+        transport.stop()
+        setTimeout(doIt, 100)
+    } else {
+        doIt()
+    }    
 })
 
 packetManager.listen('jump-to-playback-start-time', () => {
