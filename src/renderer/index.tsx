cd95a4cf2040472130de2750bade7f8979f25714
Andy Shand <andrewshand94@gmail.com>
2021-04-15 16:22:18 +0100

    Add custom scrollbars

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -60,6 +60,25 @@ const GlobalStyle = css`
             color: white;
         }
     }
+    
+    ::-webkit-scrollbar {
+        width: 20px;
+    }
+    ::-webkit-scrollbar-corner {
+        background: rgba(0,0,0,0);
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: #555;
+        border-radius: 6px;
+        border: 4px solid rgba(0,0,0,0);
+        background-clip: content-box;
+        min-width: 32px;
+        min-height: 32px;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: rgba(0,0,0,0);
+    }
+    
         .react-contextmenu {
             background-color: #222;
             background-clip: padding-box;
