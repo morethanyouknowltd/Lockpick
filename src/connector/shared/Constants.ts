f4943f38558be3fb5ad8ae559f6fb54f490648aa
Andrew Shand <andrewshand94@gmail.com>
2021-06-02 19:56:30 +0100

    v0.0.12

--- src/connector/shared/Constants.ts
+++ src/connector/shared/Constants.ts
@@ -1,6 +1,6 @@
 export const WEBSOCKET_PORT = 8181
 export const SOCKET_PORT = 8888
 
-export const APP_VERSION = '0.0.11'
+export const APP_VERSION = '0.0.12'
 export const APP_NAME = 'Lockpick'
 export const MTYK_API_ROOT = 'https://api.morethanyouknow.co.uk'
